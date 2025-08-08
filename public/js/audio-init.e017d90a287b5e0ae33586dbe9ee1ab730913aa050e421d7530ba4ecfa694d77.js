545e83eb1a3fbb6aff9ffd4d1e197fb86e5b2457
DewofyourYouth <jacobshore@gmail.com>
2025-08-08 13:49:59 +0300

    download and volume option

--- public/js/audio-init.e017d90a287b5e0ae33586dbe9ee1ab730913aa050e421d7530ba4ecfa694d77.js
+++ public/js/audio-init.e017d90a287b5e0ae33586dbe9ee1ab730913aa050e421d7530ba4ecfa694d77.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('audio.js-player').forEach(el => {
     if (!el.dataset.plyr) new Plyr(el, {
-      controls: ['play','progress','current-time', 'volume', 'download'],
+      controls: ['play','progress','current-time', 'download', "volume"],
       seekTime: 5
     });
   });
