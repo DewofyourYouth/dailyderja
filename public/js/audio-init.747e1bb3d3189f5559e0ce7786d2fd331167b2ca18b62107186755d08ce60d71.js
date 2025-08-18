02b4510e11e5e7897d00b07ea87b43ff9b8bb61d
DewofyourYouth <jacobshore@gmail.com>
2025-08-18 13:50:00 +0300

    add ask me anything remove public

--- public/js/audio-init.747e1bb3d3189f5559e0ce7786d2fd331167b2ca18b62107186755d08ce60d71.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('audio.js-player').forEach(el => {
-    if (!el.dataset.plyr) new Plyr(el, {
-      controls: ['play','progress','current-time', 'download'],
-      seekTime: 5
-    });
-  });
-});
